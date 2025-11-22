5c744d551d1e5493e1f70cd3e074b78e48c50796
anshyadav4494 <anshyadav4494@gmail.com>
2025-11-22 15:53:43 +0530

    feat: Add React plugin and configure base path for deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: ".",
-  build: {
-    outDir: "dist",
-  }
-});
+  plugins: [react()],
+  base: '/clothing_store/', // <-- EXACT repo name
+})
