0cc22d454fccfd39a16d0d5250234b2c3b16f491
anshyadav4494 <anshyadav4494@gmail.com>
2025-11-22 15:57:59 +0530

    feat: Add Vite root and build output directory configurations.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/clothing_store/', // <-- EXACT repo name
-})
+  root: ".",                      // project root
+  plugins: [react()],             // React plugin
+  base: '/clothing_store/',       // GitHub repo name (IMPORTANT)
+  build: {
+    outDir: "dist",               // build folder
+  },
+});
